20aecb42320316e40df6c13795a9b2fa76361482
Anuj <padhiyaranuj742000@gmail.com>
2021-02-01 21:47:52 +0530

    1Feb

---
+++ phppractice/1Feb/unset4.php
@@ -0,0 +1,13 @@
+<?php 
+
+//delete session
+//session_start();
+//unset($_SESSION['username']);     //for remove specific variable
+//session_destroy();                //for remove all variable
+
+//delete cookies
+//setcookie('username','alex',time()-100);
+
+
+
+?>
